655798e5ccfc1cb2d817ef97197b5c7d170a91f2
Blake Montgomery <blakemontgomery312@gmail.com>
2022-05-11 13:59:15 -0500

    Chat, VideoChat, Messaging plus Rooms

--- server.js
+++ server.js
@@ -19,7 +19,14 @@ const wss = new WebSocketServer({
 
 
 // List of currentSockets;
+// ALL sockets (not sure of use of this )
 wss.currentSockets = [];
+
+// ALL Rooms
+// an object with roomIDs as the keys, value is an array containing
+// current sockets for that room
+wss.currentRooms = {};
+
 
 // method to send all clients a current users list
 wss.updateUsers = function() {
@@ -50,17 +57,43 @@ wss.sendRemoveUserMessage = function(id) {
 
 // LISTENERS
 wss.on('connection', (socket, request, client) => {
-    console.log('a socket has been established has been made');
+    console.log('a socket has been established');
 
     socket.on('message', (message)=>{
         message = JSON.parse(message);
+        let allSockets;
+        let roomSockets;
 
+        // START HERE 
+        // You receive a new user message, it should contain what room that user is joining
+        // if the room is not in current rooms( ie is NEW), add it and add user to array
+        // if room is current, add user to array
+        // also? add user to current user (this functionality already exists)
+        // then we must pass users list of current users ONLY FOR THE ROOM THEY ARE IN
         switch(message.type){
+            // newUser = {userName: userName, userID: id, room: room}
             case 'new user':
                 // set the socketID to match the peerID
                 socket.id = message.userObj.userID;
-
+                socket.room = message.userObj.room;
                 console.log("new user message!");
+
+                // add user to the appropriate room
+                // if room is new, add to room list
+                console.log(message.userObj.room);
+                console.log(message.userObj.room in wss.currentRooms);
+                if (message.userObj.room in wss.currentRooms){
+                    console.log('hihi')
+                    const prevOccupants = wss.currentRooms[message.userObj.room];  
+                    wss.currentRooms = {...wss.currentRooms, [message.userObj.room]: [...prevOccupants, {userName: message.userObj.userName, userID: message.userObj.userID}]};
+                    
+                } else {
+                    console.log('new room');
+                                  
+                    wss.currentRooms = {...wss.currentRooms, [message.userObj.room]: [{userName: message.userObj.userName, userID: message.userObj.userID}]};
+                }
+                console.log(wss.currentRooms);
+
                 // add new user to current sockets list
                 const newSocket = {
                     userName: message.userObj.userName,
@@ -68,26 +101,40 @@ wss.on('connection', (socket, request, client) => {
                 }
                 wss.currentSockets.push(newSocket);
 
+
+
+
+                // NOW THAT SERVER LISTS HAVE BEEN UPDATED
+                // COMMUNICATE TO THE USERS OF ***THAT ROOM***
                 // make current users list
                 const currentUsers = {
                     type: 'update users',
-                    users: wss.currentSockets,
+                    users: wss.currentRooms[message.userObj.room],
                 }
 
                 // send new user message to all but sender
                 // send updated users to all
 
-                // so for each client:
-                for (let client of wss.clients){
-                    // if you are not the new user...
-                    if (client.id !== message.userObj.userID){
-                        // you get the new user message
-                        // which will initiate video sharing
-                        client.send(JSON.stringify(message));
+
+                // you need the actual sockets to send messages
+                // to get the list of sockets, call on wss.clients
+                allSockets = wss.clients;
+                roomSockets = []
+                allSockets.forEach(socket => {
+                    if (socket.room === message.userObj.room){
+                        roomSockets.push(socket);
                     }
-                    // and all clients (including new user)
-                    // get updated client list 
-                    client.send(JSON.stringify(currentUsers));
+                });
+
+                for (let socket of roomSockets){
+                    // if user is NOT the new user...
+                    if (socket.id !== message.userObj.userID){
+                        // send new user message
+                        // (which initiates video sharing)
+                        socket.send(JSON.stringify(message));
+                    }
+                    // and all users (including new user) get updated client list 
+                    socket.send(JSON.stringify(currentUsers));
                 }
                 break;
 
@@ -97,13 +144,24 @@ wss.on('connection', (socket, request, client) => {
                 // (sender has already attached message to their local chat)
                 // (clients listen for incoming message and add to their local chat)
                 console.log('chat message received');
+                let chatRoom = message.signedMessage.from.room;
                 console.log(message);
-                for (let client of wss.clients){
-                    let clientID = client.id;
-                    let senderID = message.signedMessage.from.userID;
-                    if (clientID !== senderID){
-                        console.log("distributing message");
-                        client.send(JSON.stringify(message));
+
+                allSockets = wss.clients;
+                roomSockets = []
+                allSockets.forEach(socket => {
+                    if (socket.room === chatRoom){
+                        roomSockets.push(socket);
+                    }
+                });
+
+                for (let socket of roomSockets){
+                    // if socket is NOT the sender of the message...
+                    if (socket.id !== message.signedMessage.from.userID){
+                        // send new user message
+                        // (which initiates video sharing)
+                        console.log('sending once');
+                        socket.send(JSON.stringify(message));
                     }
                 }
                 break;
